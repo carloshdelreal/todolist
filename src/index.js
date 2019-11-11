5f2ad5adb7a538c8d0d280b694a17422eed51f8a
trox115 <tofernandes67@gmail.com>
2019-11-11 18:43:24 +0000

    Added layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-var hello = 'hello world';
-
-console.log(hello);
+function addTask(){
+  let task= document.getElementById('item').value;
+  if (task){
+  console.log(task)
+}
+}
+document.getElementById('add').addEventListener('click', addTask);
