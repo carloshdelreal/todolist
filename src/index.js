af9a5a715ab4ebc0ff6f5a7377181eea75bf33af
Carlos Del Real <carloshdelreal@gmail.com>
2019-11-11 14:35:37 -0500

    refactor without images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-function addTask(){
-  let task= document.getElementById('item').value;
-  if (task){
-  console.log(task)
-}
+import './style.scss';
+
+function addTask() {
+  let task = document.getElementById('item').value;
+  if (task) {
+    console.log(task);
+  }
 }
 document.getElementById('add').addEventListener('click', addTask);
