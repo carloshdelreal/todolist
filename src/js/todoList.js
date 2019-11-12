fa254e8c781e05974972abb8c26ca198576a941e
Carlos Del Real <carloshdelreal@gmail.com>
2019-11-11 23:03:46 -0500

    finish form structure

--- src/js/todoList.js
+++ src/js/todoList.js
@@ -3,31 +3,38 @@ import bulletField from './bulletField';
 
 export default function createForm() {
   const todoContainer = createDiv(['todoContainer']);
-  const todoFields = createDiv(['form']);
+  const todoFields = createElem('form', '', ['form']);
 
   // Title
   const todoFieldTitle = createDiv(['form-group']);
-  const todoTitleLabel = createElem('label', '', []);
-  todoTitleLabel.for = 'todoTitle';
-  const todoTitle = createElem('input', 'todoTitle', ['title']);
+  const todoTitle = createElem('input', 'todoTitle', ['title', 'form-control']);
   todoTitle.placeholder = 'Todo';
-  todoFieldTitle.appendChild(todoTitleLabel);
   todoFieldTitle.appendChild(todoTitle);
 
   // description
   const todoFieldDescription = createDiv(['form-group']);
-  const todoDescription = createElem('textarea', '', ['description']);
+  const todoDescription = createElem('textarea', '', [
+    'description',
+    'form-control',
+  ]);
   todoDescription.placeholder = 'Description';
   todoFieldDescription.appendChild(todoDescription);
 
   // date
   const todoFieldDate = createDiv(['form-group']);
-  const todoDueDate = createElem('input', '', ['due-date']);
+  const todoDueDate = createElem('input', '', ['due-date', 'form-control']);
   todoDueDate.type = 'date';
   todoFieldDate.appendChild(todoDueDate);
 
+  // priority label
+  const todoFieldPriorityLab = createElem('div', 'priority', ['form-group']);
+  const todoFieldPriorityLabel = createElem('label', '', ['form-check-label']);
+  todoFieldPriorityLabel.setAttribute('for', 'priority');
+  todoFieldPriorityLabel.innerText = 'Priority';
+  todoFieldPriorityLab.appendChild(todoFieldPriorityLabel);
+
   // priority
-  const todoFieldPriority = createDiv(['form-group']);
+  const todoFieldPriority = createElem('div', '', ['form-group']);
   todoFieldPriority.appendChild(bulletField(1));
   todoFieldPriority.appendChild(bulletField(2));
   todoFieldPriority.appendChild(bulletField(3));
@@ -42,6 +49,7 @@ export default function createForm() {
   todoFields.appendChild(todoFieldTitle);
   todoFields.appendChild(todoFieldDescription);
   todoFields.appendChild(todoFieldDate);
+  todoFields.appendChild(todoFieldPriorityLab);
   todoFields.appendChild(todoFieldPriority);
   todoFields.appendChild(todoBtn);
 
