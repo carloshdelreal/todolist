781c271983651019a1165dc4ab07deef2f2d7349
trox115 <tofernandes67@gmail.com>
2019-11-20 16:37:45 +0000

    Todos design cards

--- src/js/updateList.js
+++ src/js/updateList.js
@@ -22,7 +22,16 @@ function createListTodos(todos) {
   const list = createElem('ul', '', ['todos', 'list-group']);
   todos.forEach((elem) => {
     const item = createElem('li', '', ['todoItem']);
-    item.innerText = elem.title+' Due date:'+ elem.date;
+    const card = createElem('div', '', ['card']);
+    const body = createElem('div', '', ['card-body']);
+    const title = createElem('h5', '', ['card-title']);
+    const duedate = createElem('p', '', ['card-text']);
+    title.innerText = elem.title;
+    duedate.innerText =' Due date:'+ elem.date;
+    body.appendChild(title);
+    body.appendChild(duedate);
+    card.appendChild(body);
+    item.appendChild(card);
     list.appendChild(item);
   });
   const listContainer = document.querySelector('.todos');
