138ed042f815738cbf20d7a44e6e403ca8cf5823
Carlos Del Real <carloshdelreal@gmail.com>
2019-11-21 17:25:04 -0500

    fix linting issues

--- src/js/editTodo.js
+++ src/js/editTodo.js
@@ -1,32 +1,38 @@
-import {getBulletsValue} from './todoFormData'
-function loadData(currentTodo,i){
+import { getBulletsValue } from './todoFormData';
+function loadData(currentTodo, i) {
   const form = document.querySelector('.formTodo1');
-  form.style.display = "block";
-  document.querySelectorAll('.formTodo1 .form-group input')[0].value= currentTodo.title;
+  form.style.display = 'block';
+  document.querySelectorAll('.formTodo1 .form-group input')[0].value =
+    currentTodo.title;
 
   document.querySelector('.hidden').value = i;
 
-  document.querySelectorAll('.formTodo1 .form-group textarea')[0].value= currentTodo.description;
+  document.querySelectorAll('.formTodo1 .form-group textarea')[0].value =
+    currentTodo.description;
 
-  document.querySelectorAll('.formTodo1 .form-group input')[2].value= currentTodo.date;
+  document.querySelectorAll('.formTodo1 .form-group input')[2].value =
+    currentTodo.date;
 
   const bullets = document.querySelectorAll(
     '.formTodo1 .bullets .form-check-input'
   );
-  bullets[currentTodo.priority-1].checked = true;
-
+  bullets[currentTodo.priority - 1].checked = true;
 }
 
-function updateData(){
-  const title = document.querySelectorAll('.formTodo1 .form-group input')[0].value
-  const description=  document.querySelectorAll('.formTodo1 .form-group textarea')[0].value
-  const date=  document.querySelectorAll('.formTodo1 .form-group input')[2].value;
+function updateData() {
+  const title = document.querySelectorAll('.formTodo1 .form-group input')[0]
+    .value;
+  const description = document.querySelectorAll(
+    '.formTodo1 .form-group textarea'
+  )[0].value;
+  const date = document.querySelectorAll('.formTodo1 .form-group input')[2]
+    .value;
   const bullets = document.querySelectorAll(
     '.formTodo1 .bullets .form-check-input'
   );
   const priority = getBulletsValue(bullets);
-const index = document.querySelector('.hidden').value
-  return {title,description,date,priority,index}
+  const index = document.querySelector('.hidden').value;
+  return { title, description, date, priority, index };
 }
 
-export {loadData,updateData}
+export { loadData, updateData };
